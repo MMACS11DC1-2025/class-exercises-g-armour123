d47ec1ae029c2a6d18a9f999762ef3b2c57f50a8
g-armour123 <gabriel.armour@gmail.com>
2025-11-03 22:40:22 +0000

    created recursive function, added simple user input, addec comments, next commit will have dictionary with settings

--- 4.3/recursion_exercises.py
+++ 4.3/recursion_exercises.py
@@ -6,8 +6,9 @@ Write recursive functions for each of the following problems:
 # Exercise 0: Factorial
 # Remember that n! is defined as n * (n-1)! and 0! is 1
 def factorial(n):
-    # TODO implement this
-    return 1
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
 
 print(factorial(1)) # Expected output: 1
 print(factorial(2)) # Expected output: 2
@@ -20,9 +21,9 @@ print(factorial(5)) # Expected output: 120
 # Remember that x^y is defined as x * x^(y-1) and x^0 is 1.
 
 def power(x, y):
-    # TODO implement this
-    return 1
-
+    if y == 0:
+        return 1
+    return x * power(x, y - 1)
 # Test cases
 print(power(2, 3))  # Expected output: 8
 print(power(5, 0))  # Expected output: 1
