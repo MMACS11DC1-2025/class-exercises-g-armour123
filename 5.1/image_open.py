89478b32bef20975020e63619f807c3dd9df5384
g-armour123 <gabriel.armour@gmail.com>
2025-11-12 21:30:18 +0000

    done

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -2,3 +2,15 @@ from PIL import Image
 
 image_green = Image.open("CS11/5.1/kid-green.jpg").load()
 image_beach = Image.open("CS11/5.1/beach.jpg").load()
+def isGreen(r, g, b):
+    if r <= 25 and r >= 0 and g >= 230 and g <= 255 and b >= 0 and b <= 25:
+        return True
+    else:
+        return False
+print(image_green[0, 0])
+pixel = image_green[0, 0]
+r = pixel[0]
+g = image_green[0, 0][1]
+b = image_green[0, 0][2]
+
+print(isGreen(r, g ,b))
