12c432df6fb40b83ea6081f5fd666db75f635117
g-armour123 <gabriel.armour@gmail.com>
2025-11-12 21:35:11 +0000

    did yellow as my color

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -2,15 +2,15 @@ from PIL import Image
 
 image_green = Image.open("CS11/5.1/kid-green.jpg").load()
 image_beach = Image.open("CS11/5.1/beach.jpg").load()
-def isGreen(r, g, b):
-    if r <= 25 and r >= 0 and g >= 230 and g <= 255 and b >= 0 and b <= 25:
-        return True
+def isYellow(r, g, b):
+    if r <= 255 and r >= 230 and g >= 230 and g <= 255 and b >= 0 and b <= 25:
+        return "Yellow"
     else:
-        return False
+        return "Not yellow"
 print(image_green[0, 0])
 pixel = image_green[0, 0]
 r = pixel[0]
 g = image_green[0, 0][1]
 b = image_green[0, 0][2]
 
-print(isGreen(r, g ,b))
+print(isYellow(r, g ,b))
