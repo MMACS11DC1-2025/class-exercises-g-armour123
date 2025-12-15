556979bfd393d15d192f2ff10cd38d55c78e7af0
g-armour123 <gabriel.armour@gmail.com>
2025-12-15 21:05:16 +0000

    added selection sort, clear comments, and binary search. 4th commit

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,12 +1,53 @@
 from PIL import Image
 import time
 
+#Function to detect rust pixels
 def isRusty(r, g, b):
-    return r > 120 and g > 80 and b < 80 
+    return r > 120 and g > 80 and b < 80
 
+#Function to detect non-rust pixels (clean metal or background)
 def isNotRusty(r, g, b):
-    return r > 100 and g > 100 and b > 120 or r < 50 and g < 50 and b < 50
+    return (r > 100 and g > 100 and b > 120) or (r < 50 and g < 50 and b < 50)
 
+#Convert rust percentage into a rust level (1–4)
+def getRustLevel(rustPercent):
+    if rustPercent < 10:
+        return 1
+    elif rustPercent < 30:
+        return 2
+    elif rustPercent < 60:
+        return 3
+    else:
+        return 4
+
+#Selection Sort function (highest rust level to lowest)
+def selectionSort(data):
+    for i in range(len(data)):
+        maxIndex = i
+        for j in range(i + 1, len(data)):
+            if data[j][2] > data[maxIndex][2]:
+                maxIndex = j
+        data[i], data[maxIndex] = data[maxIndex], data[i]
+
+#Binary Search for a specific rust level
+def binarySearch(data, targetLevel):
+    startIndex = 0
+    endIndex = len(data) - 1
+
+    while startIndex <= endIndex:
+        mid = (startIndex + endIndex) // 2
+        midLevel = data[mid][2]
+
+        if midLevel == targetLevel:
+            return True
+        elif midLevel < targetLevel:
+            endIndex = mid - 1
+        else:
+            startIndex = mid + 1
+
+    return False
+
+#List of images to analyze
 images = [
     "6.7/images/BlackRustSpots.jpg",
     "6.7/images/BrownishRust.jpg",
@@ -19,32 +60,70 @@ images = [
     "6.7/images/SlightlyRusted.jpg",
     "6.7/images/SpotsOfRust.jpg"
 ]
+
 results = []
 
+#Start timing
+t0 = time.time()
+
+#Process each image
 for files in images:
-    file = Image.open(files)
-    img = file.load()
-    width = file.width
-    height = file.height
+    image = Image.open(files)
+    pixels = image.load()
+    width = image.width
+    height = image.height
 
     rustyPixels = 0
     notRustyPixels = 0
-    t0 = time.time()
-    for i in range(width):
-        for j in range(height):
-            r, g, b = img[i, j]
+
+    for x in range(width):
+        for y in range(height):
+            r, g, b = pixels[x, y]
             if isRusty(r, g, b):
                 rustyPixels += 1
             elif isNotRusty(r, g, b):
                 notRustyPixels += 1
 
     totalPixels = rustyPixels + notRustyPixels
-    rustinessPercent = rustyPixels / totalPixels * 100
 
-    results.append((files, rustinessPercent))
+    if totalPixels > 0:
+        rustinessPercent = (rustyPixels / totalPixels) * 100
+    else:
+        rustinessPercent = 0
+
+    rustLevel = getRustLevel(rustinessPercent)
+
+    #Store filename, percentage, and rust level
+    results.append((files, rustinessPercent, rustLevel))
+
+#End timing
 t1 = time.time()
 totalTime = t1 - t0
+
 print("Processed {} images in {:.3f} seconds".format(len(images), totalTime))
 
+#Sort results by rust level
+selectionSort(results)
 
-    
+print("\nImages sorted by rust level (highest to lowest):")
+for item in results:
+    print(item)
+
+#Top 5 using list slicing
+print("\nTop 5 most rusted images:")
+topFive = results[:5]
+for item in topFive:
+    print(item)
+
+#Binary Search
+targetLevel = 1
+
+print("\nSearching for rust level: " + str(targetLevel))
+
+if binarySearch(results, targetLevel):
+    print("Images with rust level " + str(targetLevel) + ":")
+    for item in results:
+        if item[2] == targetLevel:
+            print(item)
+else:
+    print("No images found with that rust level.")
